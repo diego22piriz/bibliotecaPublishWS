195aca42c2faa862be3c84e82763531e762a1194
lucasb1010 <lucasb1010ps4@gmail.com>
2025-08-12 12:59:57 -0300

    Se añade geters de los datatypes

--- src/main/java/datatypes/DtDimension.java
+++ src/main/java/datatypes/DtDimension.java
@@ -1,7 +1,18 @@
 package datatypes;
 
 public class DtDimension {
+
     int length;
     int width;
     int depth;
+
+    public int getLength() {
+        return this.length;
+    }
+    public int getWidth() {
+        return this.width;
+    }
+    public int getDepth() {
+        return this.depth;
+    }
 }
--- src/main/java/datatypes/DtFecha.java
+++ src/main/java/datatypes/DtFecha.java
@@ -4,4 +4,15 @@ public class DtFecha {
     int day;
     int month;
     int year;
+
+    public int getDay() {
+        return this.day;
+    }
+    public int getMonth() {
+        return this.month;
+    }
+    public int getYear() {
+        return this.year;
+    }
 }
+
