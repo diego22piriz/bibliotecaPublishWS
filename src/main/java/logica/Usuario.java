4cf31f679c4587f53574a56bbcf6df76d117609d
lucasb1010 <lucasb1010ps4@gmail.com>
2025-08-12 13:17:27 -0300

    Se añade calse Usuario con geters y seters

--- src/main/java/logica/Usuario.java
+++ src/main/java/logica/Usuario.java
@@ -1,1 +1,33 @@
-Archivo temporal
+package logica;
+
+public class Usuario {
+
+	private String nombre;
+	private String correo;
+
+    public Usuario() {
+		super();
+	}
+
+    public Usuario(String nombre, String correo) {
+		super();
+        this.nombre = nombre;
+        this.correo = correo;
+	}
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+}
