291bc2f5e6ea11c95d1c13a56009242374b46bed
diego22piriz <diegopirizmar@gmail.com>
2025-08-29 20:17:25 -0300

    creo la clase Articulo

---
+++ src/main/java/logica/Articulo.java
@@ -0,0 +1,58 @@
+package logica;
+
+import datatypes.DtFecha;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "articulos")
+public class Articulo extends Material {
+    
+    // Atributos específicos de Articulo según el diagrama UML
+    @Column(name = "descripcion", nullable = false, length = 500)
+    private String descripcion;
+    
+    @Column(name = "peso_kg", nullable = false)
+    private float pesoKg;
+    
+    @Column(name = "dimensiones", nullable = false, length = 100)
+    private String dimensiones;
+
+    // Constructor por defecto requerido por JPA
+    public Articulo() {
+        super();
+    }
+
+    // Constructor con parámetros
+    public Articulo(String id, DtFecha fechaIngreso, String descripcion, float pesoKg, String dimensiones) {
+        super(id, fechaIngreso);
+        this.descripcion = descripcion;
+        this.pesoKg = pesoKg;
+        this.dimensiones = dimensiones;
+    }
+
+    // Getters
+    public String getDescripcion() {
+        return this.descripcion;
+    }
+
+    public float getPesoKg() {
+        return this.pesoKg;
+    }
+
+    public String getDimensiones() {
+        return this.dimensiones;
+    }
+
+    // Setters
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setPesoKg(float pesoKg) {
+        this.pesoKg = pesoKg;
+    }
+
+    public void setDimensiones(String dimensiones) {
+        this.dimensiones = dimensiones;
+    }
+}
